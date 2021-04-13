9f10cfdd90acdf30808130bd32ab1d633f88796d
Joey LeGrand <jrlegrand@gmail.com>
2021-04-13 11:35:09 +0000

    Added window query to prefer MIN over IN

--- rx_api.py
+++ rx_api.py
@@ -13,6 +13,16 @@ rxcui_ndc_match = rxcui_ndc_matcher(rxcui_list)
 rxcui_list = rxcui_ndc_match['medication_product_rxcui'].drop_duplicates().tolist()
 rxcui_ndc_match = rxcui_ndc_matcher(rxcui_list)
 
+#Prefer MIN over IN to get down to only distinct NDCs
+#https://pandas.pydata.org/pandas-docs/stable/getting_started/comparison/comparison_with_sql.html#top-n-rows-per-group
+rxcui_ndc_match = rxcui_ndc_match.assign(
+    rn = rxcui_ndc_match.sort_values(['medication_ingredient_tty'], ascending=False)
+    .groupby(['medication_ndc'])
+    .cumcount()
+    + 1
+).query('rn < 2').drop(columns=['rn'])
+
+
 #TODO: Pandas mugging similar to windows functions
 
 #saves df to csv
