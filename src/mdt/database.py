73dd0e9da98512c5283e02243d51e9be12ece4c0
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-05-07 12:45:00 -0500

    Add load_meps function to database.py

--- src/mdt/database.py
+++ src/mdt/database.py
@@ -1,7 +1,8 @@
-from .rxnorm.utils import get_dataset
-
+from . import rxnorm, meps
 from pathlib import Path
-import zipfile,io, sqlite3
+import zipfile
+import io
+import sqlite3
 import pandas as pd
 
 
@@ -59,7 +60,7 @@ def read_sql_string(file_name):
 def load_rxnorm():
     """downloads and loads RxNorm dataset into database"""
 
-    z = zipfile.ZipFile(get_dataset(handler=io.BytesIO))
+    z = zipfile.ZipFile(rxnorm.utils.get_dataset(handler=io.BytesIO))
 
     col_names = ['RXCUI','LAT','TS','LUI','STT','SUI','ISPREF','RXAUI','SAUI','SCUI','SDUI','SAB','TTY','CODE','STR','SRL','SUPPRESS','CVF','test']
     rxnconso = pd.read_csv(z.open('rrf/RXNCONSO.RRF'),sep='|',header=None,dtype=object,names=col_names)
@@ -77,3 +78,61 @@ def load_rxnorm():
     del rxnsat 
 
     del z
+
+
+def load_meps():
+    '''Load Meps data into db'''
+    z = zipfile.ZipFile(
+        meps.utils.get_dataset('h206adat.zip', handler=io.BytesIO)
+    )
+
+    meps_prescription = pd.read_fwf(
+        z.open('H206A.dat'),
+        header=None,
+        names=meps.columns.p_col_names,
+        converters={col: str for col in meps.columns.p_col_names},
+        colspecs=meps.columns.p_col_spaces,
+    )
+
+    sql_create_table('meps_prescription', meps_prescription)
+    del meps_prescription
+    del z
+
+    z = zipfile.ZipFile(
+        meps.utils.get_dataset('h209dat.zip', handler=io.BytesIO)
+    )
+
+    meps_demographics = pd.read_fwf(
+        z.open('h209.dat'),
+        header=None,
+        names=meps.columns.d_col_names,
+        converters={col: str for col in meps.columns.d_col_names},
+        colspecs=meps.columns.d_col_spaces,
+        usecols=['DUPERSID', 'PERWT18F', "REGION18", 'SEX', 'AGELAST']
+    )
+
+    # removing numbers from meps_demographic column names, since the '18' in region18 and perwt18f in MEPS are year-specific
+    meps_demographics.columns = meps_demographics.columns.str.replace(r'\d+', '',regex=True)
+    sql_create_table('meps_demographics', meps_demographics)
+    del meps_demographics
+    del z
+
+    sql_create_table('meps_region_states', meps.columns.meps_region_states)
+
+    meps_reference_str = read_sql_string('meps_reference.sql')
+    meps_reference = db_query(meps_reference_str)
+    sql_create_table('meps_reference', meps_reference)
+    del meps_reference
+
+    # TEST!!!!!!!!!!!!!!!! reads record count from created database
+    meps_prescription = db_query("Select count(*) AS records from meps_prescription")
+    print('DB table meps_prescription  has {0} records'.format(meps_prescription['records'].iloc[0]))
+
+    meps_demographics = db_query("Select count(*) AS records from meps_demographics")
+    print('DB table meps_demographics has {0} records'.format(meps_demographics['records'].iloc[0]))
+
+    meps_reference = db_query("Select count(*) AS records from meps_reference")
+    print('DB table meps_reference has {0} records'.format(meps_reference['records'].iloc[0]))
+
+    meps_region_states = db_query("Select count(*) AS records from meps_region_states")
+    print('DB table meps_region_states has {0} records'.format(meps_region_states['records'].iloc[0]))
