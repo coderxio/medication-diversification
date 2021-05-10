1d2fbf4dce26a55c5ee72eb164bd090ab7ae55c7
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-05-10 07:51:10 -0500

    Import lib requires python >3.7

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # keywords='sample, setuptools, development',  # Optional
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
-    python_requires='>=3.6, <4',
+    python_requires='>=3.7 <4',
     install_requires=[
         'requests',
         'pandas'
